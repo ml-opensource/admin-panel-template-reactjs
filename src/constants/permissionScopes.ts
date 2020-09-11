0d85827ddbbda11ccd5c6388af36ee067bb98d60
Themi Tsiotas von Pfaler <themi1234@gmail.com>
2020-09-11 13:53:08 +0200

    add enum with permission scopes

---
+++ src/constants/permissionScopes.ts
@@ -0,0 +1,10 @@
+/**
+ * All permission scopes.
+ * The scopes should be aligned with backend
+ */
+export enum PermissionEnum {
+  DASHBOARD = "dashboard",
+  USERS_READ = "users:read",
+  USERS_WRITE = "users:write",
+  USERS_DELETE = "users:delete",
+}
