cf974a59fac004a9f68a0a99b55f490d942d9775
Themi Tsiotas von Pfaler <themi1234@gmail.com>
2020-09-23 10:26:51 +0200

    update eslint rules and vscode settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,10 +29,16 @@ module.exports = {
     ],
     "react/jsx-props-no-spreading": 0,
     "import/prefer-default-export": 0,
+    // no return types needed if it can be inferred. useful for react components and sagas so it's less to worry about
+    "@typescript-eslint/explicit-function-return-type": "off",
     "no-restricted-imports": [
       "error",
       {
-        patterns: ["@material-ui/*/*/*", "!@material-ui/core/test-utils/*"],
+        patterns: [
+          "@material-ui/*/*/*",
+          "!@material-ui/core/test-utils/*",
+          "features/*/*/*",
+        ],
       },
     ],
   },
