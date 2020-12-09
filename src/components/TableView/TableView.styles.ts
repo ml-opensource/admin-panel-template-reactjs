7c1cfd05ad9e3a6ba825527a10d070b1d6714354
Yuki Noda <yukinoda08@gmail.com>
2020-12-09 12:02:05 +0900

    added pagination

--- src/components/TableView/TableView.styles.ts
+++ src/components/TableView/TableView.styles.ts
@@ -1,7 +1,16 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 
-export const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
-});
+export const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: "100%",
+    },
+    paper: {
+      width: "100%",
+      marginBottom: theme.spacing(2),
+    },
+    table: {
+      minWidth: 650,
+    },
+  })
+);
--- src/components/TableView/TableView.tsx
+++ src/components/TableView/TableView.tsx
@@ -1,9 +1,10 @@
-import React, { FC, memo } from "react";
+import React, { FC, memo, useState } from "react";
 import Table, { TableProps } from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import { useStyles } from "./TableView.styles";
@@ -18,43 +19,77 @@ interface Data {
 
 interface TableViewProps {
   tableProps: TableProps;
-  data: Data[];
+  rows: Data[];
 }
 
-const TableView: FC<TableViewProps> = ({ data = [], tableProps }) => {
+const TableView: FC<TableViewProps> = ({ rows = [], tableProps }) => {
   const classes = useStyles();
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const emptyRows =
+    rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
 
   return (
-    <TableContainer component={Paper}>
-      <Table
-        className={classes.table}
-        aria-label="simple table"
-        {...tableProps}
-      >
-        <TableHead>
-          <TableRow>
-            <TableCell>id</TableCell>
-            <TableCell align="right">Name</TableCell>
-            <TableCell align="right">Year</TableCell>
-            <TableCell align="right">Color</TableCell>
-            <TableCell align="right">Pantone Value</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map(row => (
-            <TableRow key={row.id}>
-              <TableCell component="th" scope="row">
-                {row.id}
-              </TableCell>
-              <TableCell align="right">{row.name}</TableCell>
-              <TableCell align="right">{row.year}</TableCell>
-              <TableCell align="right">{row.color}</TableCell>
-              <TableCell align="right">{row.pantone_value}</TableCell>
+    <Paper className={classes.paper}>
+      <TableContainer>
+        <Table
+          className={classes.table}
+          aria-label="simple table"
+          {...tableProps}
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell>id</TableCell>
+              <TableCell align="right">Name</TableCell>
+              <TableCell align="right">Year</TableCell>
+              <TableCell align="right">Color</TableCell>
+              <TableCell align="right">Pantone Value</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map(row => (
+                <TableRow hover key={row.id}>
+                  <TableCell component="th" scope="row">
+                    {row.id}
+                  </TableCell>
+                  <TableCell align="right">{row.name}</TableCell>
+                  <TableCell align="right">{row.year}</TableCell>
+                  <TableCell align="right">{row.color}</TableCell>
+                  <TableCell align="right">{row.pantone_value}</TableCell>
+                </TableRow>
+              ))}
+            {emptyRows > 0 && (
+              <TableRow style={{ height: 53 * emptyRows }}>
+                <TableCell colSpan={6} />
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+    </Paper>
   );
 };
 
