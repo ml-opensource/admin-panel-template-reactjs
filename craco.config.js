da22a5a249cdd6884b11ba8b43c35ef7662ff8ce
Themi Tsiotas von Pfaler <themi1234@gmail.com>
2021-01-05 18:07:14 +0100

    setup ant design theming

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const CracoAntDesignPlugin = require("craco-antd");
+const theme = require("./src/styles/antd-theme");
 
 module.exports = {
   webpack: {
@@ -6,4 +8,15 @@ module.exports = {
       "@app": path.resolve(__dirname, "./src"),
     },
   },
+  plugins: [
+    {
+      plugin: CracoAntDesignPlugin,
+      options: {
+        customizeTheme: theme,
+      },
+      lessLoaderOptions: {
+        noIeCompat: true,
+      },
+    },
+  ],
 };
---
+++ src/styles/antd-theme.js
@@ -0,0 +1,7 @@
+// https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
+module.exports = {
+  // "@primary-color": "",
+  // "@layout-header-background": "",
+  // "@layout-header-height": "auto",
+  "@layout-body-background": "#f0f2f5",
+};
