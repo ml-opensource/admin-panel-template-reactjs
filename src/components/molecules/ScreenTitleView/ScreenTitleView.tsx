9bfcafa8d6709233cecc0bd3857c16b5390b6cc9
Nael <nara@nodes.dk>
2021-01-27 14:14:29 +0000

    Removed button out of screen title view

--- src/components/molecules/ScreenTitleView/ScreenTitleView.tsx
+++ src/components/molecules/ScreenTitleView/ScreenTitleView.tsx
@@ -1,7 +1,3 @@
-import { useTranslation } from "react-i18next";
-
-import Button from "@app/components/atoms/Button/Button";
-
 import styles from "./ScreenTitleView.module.scss";
 
 interface ScreenTitleViewProps {
@@ -9,18 +5,9 @@ interface ScreenTitleViewProps {
 }
 
 const ScreenTitleView = ({ mainTitle }: ScreenTitleViewProps) => {
-  const { t } = useTranslation();
-
   return (
     <div>
       <h1 className={styles.title}>{mainTitle}</h1>
-      <div className={styles.buttonContainer}>
-        <Button
-          type="primary"
-          size="large"
-          buttonText={t("default.buttonText")}
-        />
-      </div>
     </div>
   );
 };
