e39e5e7902750ed314582216091138e084d8c13f
Themi Tsiotas von Pfaler <themi1234@gmail.com>
2021-03-26 15:53:39 +0100

    update button with no padding option

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,22 +1,40 @@
-import { Button as AntDesignButton } from "antd";
-import { ButtonProps as AntDesignButtonProps } from "antd/lib/button";
+import { memo } from "react";
+
+import { Button as AntdButton } from "antd";
+import { ButtonProps as AntdButtonProps } from "antd/es/button";
+import classnames from "classnames/bind";
 import { Link, LinkProps } from "react-router-dom";
 
-interface ButtonProps extends Omit<AntDesignButtonProps, "href"> {
+import styles from "./Button.module.scss";
+
+const cx = classnames.bind(styles);
+
+interface ButtonProps extends Omit<AntdButtonProps, "href"> {
   /**
    * Turn button into link (optional)
    */
   to?: LinkProps["to"];
+  /**
+   * Remove horizontal padding (optional)
+   */
+  noPadding?: boolean;
 }
 
-const Button = ({ to, ...buttonProps }: ButtonProps) => {
-  const buttonContent = <AntDesignButton {...buttonProps} />;
+const Button = memo(({ to, className, noPadding, ...rest }: ButtonProps) => {
+  const buttonContent = (
+    <AntdButton
+      className={cx(styles.button, className, {
+        noPadding,
+      })}
+      {...rest}
+    />
+  );
 
   if (to) {
     return <Link to={to}>{buttonContent}</Link>;
   }
 
   return buttonContent;
-};
+});
 
 export default Button;
