3330a43c52aa8b9772b69c94a36f208de1c702d3
Themi Tsiotas von Pfaler <themi1234@gmail.com>
2021-03-26 15:46:04 +0100

    move all ant design menu component into same component because of bug

--- src/components/molecules/Navigation/components/NavLeftContent/NavLeftContent.tsx
+++ src/components/molecules/Navigation/components/NavLeftContent/NavLeftContent.tsx
@@ -1,39 +1,19 @@
-import { memo } from "react";
+import React, { memo } from "react";
 
-import { Menu } from "antd";
 import { MenuMode } from "antd/lib/menu";
 import cx from "classnames";
-import { useLocation } from "react-router-dom";
 
 import { ReactComponent as Logo } from "@app/assets/images/logo.svg";
-import { PRIVATE_LIST } from "@app/routes/routes.config";
-import { RouteItemDef } from "@app/types/route.types";
 
-import NavLink from "../NavLink/NavLink";
-import NavSubMenu from "../NavSubMenu/NavSubMenu";
+import NavMenu from "../NavMenu/NavMenu";
 import styles from "./NavLeftContent.module.scss";
-
-const navLinks: RouteItemDef[] = PRIVATE_LIST.filter(
-  route => !route.hideInNavigation
-);
 
 interface NavLeftContentProps {
   mode?: MenuMode;
 }
 
 const NavLeftContent = memo(({ mode = "horizontal" }: NavLeftContentProps) => {
-  const location = useLocation();
-
   const isSidebar = mode === "inline";
-  const rootPathname = isSidebar
-    ? [...location.pathname.split(/(?=\/)/g, 1)]
-    : undefined;
-
-  const highlightMenu = [
-    ...location.pathname.split(/(?=\/)/g, 1), // Highlight root url
-    location.pathname.substr(0, location.pathname.lastIndexOf("/")), // Highlight parent url
-    location.pathname, // Highlight entire url
-  ];
 
   return (
     <>
@@ -44,24 +24,7 @@ const NavLeftContent = memo(({ mode = "horizontal" }: NavLeftContentProps) => {
       >
         <Logo className={styles.logo} />
       </div>
-      <Menu
-        mode={mode}
-        selectedKeys={highlightMenu}
-        defaultOpenKeys={rootPathname}
-        theme="dark"
-      >
-        {navLinks.map(navItem =>
-          navItem.nestedRoutes?.length ? (
-            <NavSubMenu key={navItem.id} item={navItem} isSidebar={isSidebar} />
-          ) : (
-            <Menu.Item
-              key={Array.isArray(navItem.path) ? navItem.path[0] : navItem.path}
-            >
-              <NavLink navItem={navItem} />
-            </Menu.Item>
-          )
-        )}
-      </Menu>
+      <NavMenu mode={mode} isSidebar={isSidebar} />
     </>
   );
 });
---
+++ src/components/molecules/Navigation/components/NavMenu/NavMenu.tsx
@@ -0,0 +1,108 @@
+import { DownOutlined } from "@ant-design/icons";
+import { Menu } from "antd";
+import { MenuMode } from "antd/lib/menu";
+import { useTranslation } from "react-i18next";
+import { useLocation } from "react-router-dom";
+
+import { PRIVATE_LIST } from "@app/routes/routes.config";
+import { RouteGroupDef, RouteItemDef } from "@app/types/route.types";
+
+import NavLink from "../NavLink/NavLink";
+import styles from "./NavMenu.module.scss";
+
+const navLinks: RouteItemDef[] = PRIVATE_LIST.filter(
+  route => !route.hideInNavigation
+);
+
+interface NavMenuProps {
+  isSidebar?: boolean;
+  mode?: MenuMode;
+}
+
+const NavMenu = ({ isSidebar, mode }: NavMenuProps) => {
+  const { t } = useTranslation();
+  const location = useLocation();
+
+  const rootPathname = isSidebar
+    ? [...location.pathname.split(/(?=\/)/g, 1)]
+    : undefined;
+
+  const highlightMenu = [
+    ...location.pathname.split(/(?=\/)/g, 1), // Highlight root url
+    location.pathname.substr(0, location.pathname.lastIndexOf("/")), // Highlight parent url
+    location.pathname, // Highlight entire url
+  ];
+
+  /**
+   * Ant Design has a bug, where it is NOT possible
+   * to create custom wrapper components around the Menu and its sub components.
+   * So all AntD Menu components need to be in the same file for now
+   */
+  return (
+    <Menu
+      mode={mode}
+      selectedKeys={highlightMenu}
+      defaultOpenKeys={rootPathname}
+      theme="dark"
+    >
+      {navLinks.map(navItem =>
+        navItem.nestedRoutes?.length ? (
+          <Menu.SubMenu
+            popupOffset={[-16, 7]}
+            key={Array.isArray(navItem.path) ? navItem.path[0] : navItem.path}
+            title={
+              <div className={styles.subMenuItem}>
+                <span>
+                  {navItem.navigationTitle
+                    ? t(navItem.navigationTitle)
+                    : `Missing navigationTitle for "${navItem.id}"`}
+                </span>
+                {!isSidebar && <DownOutlined className={styles.icon} />}
+              </div>
+            }
+          >
+            {navItem.nestedRoutes?.map(
+              (subItem: RouteItemDef | RouteGroupDef) =>
+                "groupTitle" in subItem ? (
+                  <Menu.ItemGroup
+                    key={subItem.id}
+                    title={t(subItem.groupTitle)}
+                  >
+                    {subItem.nestedRoutes?.map(subGroupItem => (
+                      <Menu.Item
+                        key={
+                          Array.isArray(subGroupItem.path)
+                            ? subGroupItem.path[0]
+                            : subGroupItem.path
+                        }
+                      >
+                        <NavLink navItem={subGroupItem} />
+                      </Menu.Item>
+                    ))}
+                  </Menu.ItemGroup>
+                ) : (
+                  <Menu.Item
+                    key={
+                      Array.isArray(subItem.path)
+                        ? subItem.path[0]
+                        : subItem.path
+                    }
+                  >
+                    <NavLink navItem={subItem} />
+                  </Menu.Item>
+                )
+            )}
+          </Menu.SubMenu>
+        ) : (
+          <Menu.Item
+            key={Array.isArray(navItem.path) ? navItem.path[0] : navItem.path}
+          >
+            <NavLink navItem={navItem} />
+          </Menu.Item>
+        )
+      )}
+    </Menu>
+  );
+};
+
+export default NavMenu;
