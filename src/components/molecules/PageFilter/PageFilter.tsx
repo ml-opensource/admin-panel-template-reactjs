2c00a5740d675e55dadae678c3ac9254e1a083bc
Nour Jakobsen Halabi <noha@nodes.dk>
2021-04-16 17:19:46 +0200

    Working on onChange trigger

--- src/components/molecules/PageFilter/PageFilter.tsx
+++ src/components/molecules/PageFilter/PageFilter.tsx
@@ -7,11 +7,18 @@ import FilterItem from "./components/FilterItem/FilterItem";
 interface PageFilterProps extends FormProps {
   children: React.ReactNode;
   columns?: 1 | 2 | 3 | 4 | 5 | 6;
+  onFieldChange?: boolean;
+  onSubmit?: boolean;
 }
 
 const PageFilter = ({ children, columns = 4, ...rest }: PageFilterProps) => {
   return (
-    <Form {...rest}>
+    <Form
+      {...rest}
+      onChange={() => console.log("On change")}
+      onFieldsChange={() => console.log("Fields changed")}
+      onValuesChange={() => console.log("Values changed")}
+    >
       <Row>
         {React.Children.map(children, child => (
           <Col xs={24} sm={12} lg={24 / columns}>
--- src/features/settings/screens/UsersScreen/components/UsersFilter/UsersFilter.tsx
+++ src/features/settings/screens/UsersScreen/components/UsersFilter/UsersFilter.tsx
@@ -1,18 +1,25 @@
 import { memo } from "react";
 
-import { Input } from "antd";
+import { Input, Select } from "antd";
 
 import PageFilter, {
   FilterItem,
 } from "@app/components/molecules/PageFilter/PageFilter";
 
+const { Option } = Select;
+
 const UsersFilter = () => {
   return (
     <PageFilter>
-      <FilterItem label="Test 1">
+      <FilterItem label="Test 1" name="test1">
         <Input placeholder="Test 1" />
       </FilterItem>
-      <FilterItem>Test 2</FilterItem>
+      <FilterItem label="Test 2" name="test2">
+        <Select placeholder="Test 2">
+          <Option value="john">John</Option>
+          <Option value="jane">Jane</Option>
+        </Select>
+      </FilterItem>
       <FilterItem>Test 3</FilterItem>
       <FilterItem>Test 4</FilterItem>
       <FilterItem>Test 5</FilterItem>
