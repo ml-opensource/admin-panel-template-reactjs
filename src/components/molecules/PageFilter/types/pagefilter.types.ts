0c315cf778ec4725712775720de45f858fcdccd9
Nour Jakobsen Halabi <noha@nodes.dk>
2021-04-27 10:18:42 +0200

    Formatted comments for parse props

--- src/components/molecules/PageFilter/types/pagefilter.types.ts
+++ src/components/molecules/PageFilter/types/pagefilter.types.ts
@@ -3,48 +3,53 @@ type ParseDef<T> = boolean | Array<keyof T>;
 export interface ParseFiltersProps<T> {
   /**
    * Runs through arrays in the query string and parses strings with date
-   * values. Use this when using RangePicker. If a boolean is passed, all
-   * arrays with date values will be parsed. If you want to specify which
-   * key / property to parse, you can pass it an array of strings for keys,
-   * but you will also need to pass a type containing the properties to the
-   * PageFilter.
+   * values. If a boolean is passed, all arrays with date values will be
+   * parsed. If you want to specify which key / property to parse, you can
+   * pass it an array of strings for keys, but you will also need to pass a
+   * type containing the properties to the PageFilter.
+   *
+   * **Use this when using RangePicker.**
    */
   parseArrayDates?: ParseDef<T>;
   /**
    * Runs through arrays in the query string, and parses string
-   * that contain numbers. Useful if you have a multi select, with
-   * numbers for values. If a boolean is passed, all arrays with
+   * that contain numbers. If a boolean is passed, all arrays with
    * date values will be parsed. If you want to specify which
    * key / property to parse, you can pass it an array of strings
    * for keys, but you will also need to pass a type containing
    * the properties to the PageFilter.
+   *
+   * **Useful if you have a multi select, with numbers for values.**
    */
   parseArrayNumbers?: ParseDef<T>;
   /**
    * Runs through the query string and parses string with boolean
-   * values. Use when you have checkboxes, radio buttons, or switches.
-   * If a boolean is passed, all arrays with date values will be parsed.
-   * If you want to specify which key / property to parse, you can pass
-   * it an array of strings for keys, but you will also need to pass a
-   * type containing the properties to the PageFilter.
+   * values. If a boolean is passed, all arrays with date values will
+   * be parsed. If you want to specify which key / property to parse,
+   * you can pass it an array of strings for keys, but you will also
+   * need to pass a type containing the properties to the PageFilter.
+   *
+   * **Use when you have checkboxes, radio buttons, or switches.**
    */
   parseBoolean?: ParseDef<T>;
   /**
    * Runs through query string and parses strings with date values.
-   * Use this when you date pickers. If a boolean is passed, all
-   * arrays with date values will be parsed. If you want to specify which
-   * key / property to parse, you can pass it an array of strings for keys,
-   * but you will also need to pass a type containing the properties to the
-   * PageFilter.
+   * If a boolean is passed, all arrays with date values will be parsed.
+   * If you want to specify which key / property to parse, you can pass
+   * it an array of strings for keys, but you will also need to pass a
+   * type containing the properties to the PageFilter.
+   *
+   * **Use this when using date pickers.**
    */
   parseDates?: ParseDef<T>;
   /**
    * Runs through the query string and parses strings with numbers.
-   * Use when you have fields that contain numbers for values. If a
-   * boolean is passed, all arrays with date values will be parsed.
+   * If a boolean is passed, all arrays with date values will be parsed.
    * If you want to specify which key / property to parse, you can
    * pass it an array of strings for keys, but you will also need
    * to pass a type containing the properties to the PageFilter.
+   *
+   * **Use when you have fields that contain numbers for values.**
    */
   parseNumbers?: ParseDef<T>;
 }
