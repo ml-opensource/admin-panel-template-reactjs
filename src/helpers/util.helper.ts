050990c47b84ae4402a0d95240cb4fbf91ad9458
Themi Tsiotas von Pfaler <themi1234@gmail.com>
2021-05-19 09:03:52 +0200

    add file helper for downloading single file, multiple files, and converting blob to csv

--- src/helpers/util.helper.ts
+++ src/helpers/util.helper.ts
@@ -4,3 +4,10 @@
 export const isURL = (str: string) => {
   return /http|www|\./.test(str);
 };
+
+/**
+ * A promise to delay an async function
+ * @param ms how many milliseconds to wait
+ */
+export const delay = (ms: number) =>
+  new Promise(resolve => setTimeout(resolve, ms));
