c1e9bf6c466d7cac00b2da32be5f18b1e64bf87d
Themi Tsiotas von Pfaler <themi1234@gmail.com>
2021-05-28 15:30:39 +0200

    move user helper function into util helper

--- src/helpers/user.helper.ts
+++
@@ -1,8 +0,0 @@
-export const getInitials = (name: string, maxChar: number) => {
-  return name
-    .split(/\s/)
-    .map(word => word[0])
-    .join("")
-    .substr(0, maxChar)
-    .toUpperCase();
-};
