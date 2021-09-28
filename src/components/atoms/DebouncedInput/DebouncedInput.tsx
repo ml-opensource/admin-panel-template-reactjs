6dba021bec3ce39e34ceea09fc539a1f6d6d44d4
Yuki Noda <16083322+yukinoda@users.noreply.github.com>
2021-09-28 10:17:27 +0900

    migration: Prettier `v2.2.x` to `v2.4.x` (#118)
    * upgraded prettier from `2.2.x` to `2.4.x`
    
    * fixed no files matching
    
    * prettier formatted
    
    * Revert "fixed no files matching"
    
    This reverts commit 602758f375d9451f57cd554a873114968c2be3ee.

--- src/components/atoms/DebouncedInput/DebouncedInput.tsx
+++ src/components/atoms/DebouncedInput/DebouncedInput.tsx
@@ -38,13 +38,12 @@ const DebouncedInput = ({
     wait
   );
 
-  const persistedOnChange = (persistOnChange?: React.ChangeEventHandler) => (
-    e: React.ChangeEvent
-  ) => {
-    // persist event as it will be resolved after debounce
-    e.persist();
-    persistOnChange && debouncedOnChange(e, persistOnChange);
-  };
+  const persistedOnChange =
+    (persistOnChange?: React.ChangeEventHandler) => (e: React.ChangeEvent) => {
+      // persist event as it will be resolved after debounce
+      e.persist();
+      persistOnChange && debouncedOnChange(e, persistOnChange);
+    };
 
   return (
     <Input
