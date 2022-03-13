5e06ee4c8b01d5b8bf2c1eab271ac9ef2d5ab670
Atiq <atiqur.rahman@monstar-lab.com>
2022-03-13 16:39:00 +0600

    feat: axios error type

--- src/redux/api.thunk.ts
+++ src/redux/api.thunk.ts
@@ -1,9 +1,26 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { AxiosResponse } from "axios";
+import { AxiosError, AxiosPromise } from "axios";
+
+export type ErrorType<Error> = AxiosError<Error>;
+
+export const errorHandler = <TError = Error>(error: ErrorType<TError>) => {
+  const { request, response } = error;
+  if (response) {
+    // process error to based on type
+    const message = response.data;
+    return message;
+  }
+  if (request) {
+    // request sent but no response received
+    return "network.noInternet";
+  }
+  // Something happened in setting up the request that triggered an Error
+  return "network.unknown";
+};
 
 export function createApiAsyncThunk<Result, Arg>(
   key: string,
-  action: (arg: Arg) => Promise<AxiosResponse<Result>>
+  action: (arg: Arg) => AxiosPromise<Result>
 ) {
   return createAsyncThunk<Result, Arg>(
     key,
@@ -13,7 +30,7 @@ export function createApiAsyncThunk<Result, Arg>(
 
         return data;
       } catch (error) {
-        return rejectWithValue(error.response.data);
+        return rejectWithValue(errorHandler(error));
       }
     }
   );
